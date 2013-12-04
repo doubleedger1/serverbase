a992675bed7e5b1aff0c086e4f3117c7c8e728a5
Brandon Taylor <XC1@hotmail.co.uk>
2013-12-04 12:44:49 +0000

    Update sv_commands.lua
    Changing the command names.

--- lua/sv_commands.lua
+++ lua/sv_commands.lua
@@ -1,25 +1,25 @@
 hook.Add("PlayerSay", "Commands", function(ply, text, public)
 	if ( string.sub(text, 1, 6) == "/admin" ) then
 		if ( ply:IsAdmin() ) then
-			ply:ConCommand("admin")
+			ply:ConCommand("sb_admin")
 			return ""
 		end
 	end
 	
 	if ( string.sub(text, 1, 5) == "/bans" ) then
 		if ( ply:IsAdmin() ) then
-			ply:ConCommand("suspensions")
+			ply:ConCommand("sb_suspensions")
 			return ""
 		end
 	end
 	
 	if ( string.sub(text, 1, 5) == "/shop" ) then
-		ply:ConCommand("shop")
+		ply:ConCommand("sb_shop")
 		return ""
 	end
 	
 	if ( string.sub(text, 1, 8) == "/servers" or string.sub(text, 1, 7) == "/portal" ) then
-		ply:ConCommand("portal")
+		ply:ConCommand("sb_portal")
 		return ""
 	end
 end)
