9aa5e1c04bf3db7bf4d4d2174ef8982978462e94
Brandon Taylor <XC1@hotmail.co.uk>
2014-01-02 01:17:51 +0000

    fixed some stuff
    fix

--- lua/sv_commands.lua
+++ lua/sv_commands.lua
@@ -24,13 +24,27 @@ hook.Add("PlayerSay", "Commands", function(ply, text, public)
 		return ""
 	end
 	
-	if ( string.sub(text, 1, 8) == "/votemap" ) then
-		ply:ConCommand("sb_votemap")
-		return ""
-	end
-	
 	if ( string.sub(text, 1, 6) == "/rules" ) then
 		ply:ConCommand("sb_rules")
+		return ""
+	end
+end)
+
+hook.Add("PlayerSay", "adminchat", function(ply, text, public)
+	if ( string.sub(text, 1, 3) == "@@@" ) then
+		PrintMessage(HUD_PRINTCENTER, "[MESSAGE] " ..string.sub(text, 4))
+		return ""
+	end
+	if ( string.sub(text, 1, 2) == "@@" ) then
+		PrintMessage(HUD_PRINTTALK, "[MESSAGE] " ..string.sub(text, 3))
+		return ""
+	end
+	if ( string.sub(text, 1, 1) == "@" ) then
+		for k, v in pairs (player.GetAll()) do
+			if ( v:IsAdmin() ) then
+				v:ChatPrint("[ADMIN] " ..ply:Nick()..": " ..string.sub(text, 2))
+			end
+		end
 		return ""
 	end
 end)
