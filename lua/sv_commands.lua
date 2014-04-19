d787f1a046380aa8b742c1496c76984f8d3eba53
Brandon Taylor <XC1@hotmail.co.uk>
2014-04-19 13:50:03 +0000

    Update sv_commands.lua
    Fixed the chat commands; previously it didn't put any spaces between the arguments and therefore classed it as one command. Also added the userid extra command.

--- lua/sv_commands.lua
+++ lua/sv_commands.lua
@@ -3,6 +3,7 @@ ChatCommands = {
 	admin = "sb_admin",
 	bans = "sb_suspensions",
 	servers = "sb_portal",
+	portal = "sb_portal",
 	rules = "sb_rules",
 
 	ban = "sb_ban",
@@ -33,17 +34,22 @@ hook.Add("PlayerSay", "ChatCommands", function(ply, text, public)
 		if ChatCommands[command] then
 			local cmdArgs = ""
 
-
-			for k,v in pairs(args) do
+			for k, v in pairs(args) do
 				if k > 1 then
-					cmdArgs = cmdArgs .. v .. ""
+					cmdArgs = cmdArgs .. v .. " "
 				else
-					cmdArgs = cmdArgs .. "" .. v .. ""
+					cmdArgs = cmdArgs .. " " .. v .. " "
 				end
+			end				
+			
+			if (!cmdArgs) then
+				ply:ConCommand(ChatCommands[command])
+				return ""
 			end
-
-			ply:ConCommand(ChatCommands[consoleCmd]..cmdArgs)
+			if (cmdArgs) then					
+				ply:ConCommand(ChatCommands[command]..cmdArgs)
 			return ""
+			end
 		end
 	end
 end)
@@ -53,23 +59,29 @@ hook.Add("PlayerSay", "ExtraCommands", function(ply, text, public)
 		ply:ChatPrint("The current log is: " ..tostring(os.date("%m_%d_%y")).."_"..GetConVarString("ip"):Replace(".", "_").."/log"..NUM_OF_FILES..".txt")
 		return ""
 	end
+	
+	if ( string.sub(text, 1, 7) == "/userid" and ply:IsAdmin() ) then
+		ReturnUserID(ply, string.sub(text, 9))
+		return ""
+	end
 end)
 
 hook.Add("PlayerSay", "adminchat", function(ply, text, public)
-	if ( string.sub(text, 1, 3) == "@@@" ) then
+	if ( string.sub(text, 1, 3) == "@@@" and ply:IsAdmin() ) then
 		PrintMessage(HUD_PRINTCENTER, "[MESSAGE] " ..string.sub(text, 4))
 		return ""
 	end
-	if ( string.sub(text, 1, 2) == "@@" ) then
-		PrintMessage(HUD_PRINTTALK, "[MESSAGE] " ..string.sub(text, 3))
+	if ( string.sub(text, 1, 2) == "@@" and ply:IsAdmin() ) then
+		for k, v in pairs(player.GetAll()) do
+			chat.AddText(v, COLOR_TARGET, "[MESSAGE] ", COLOR_TEXT, string.sub(text, 3))
 		return ""
+		end
 	end
 	if ( string.sub(text, 1, 1) == "@" ) then
 		for k, v in pairs (player.GetAll()) do
 			if ( v:IsAdmin() ) then
-				v:ChatPrint("[ADMIN] " ..ply:Nick()..": " ..string.sub(text, 2))
+				chat.AddText(v, COLOR_TARGET, "[ADMIN] ", COLOR_ADMIN, ply:Nick()..": ", COLOR_TEXT, string.sub(text, 2))
 			end
 		end
 		return ""
 	end
-end)
